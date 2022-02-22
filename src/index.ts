5d36b09a68e9b99aab56331d421f1c53ef4ecd55
ViniciusBirreik <viniciusbirreik@gmail.com>
2022-02-22 17:33:34 -0300

    criando API

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+
+app.get('', (request: Request, response: Response) => {
+    const {...recados} = request.query
+    
+})
+
+app.listen(4040, () => {
+    console.log('servidor rodando')
+})
